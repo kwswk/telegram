e9fc689568e55b027087ff768a1f459b8a3905be
kWs <volvokung@gmail.com>
2020-11-17 01:31:08 +0800

    telegram bot init

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def telegram_bot_sendtext(bot_message):
+    bot_token = '1460342157:AAH26oIP5bwvL6SMgx3lE4BsiQLnAiidGQ8'
+    bot_chatID = '68096939'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+0
+
+test = telegram_bot_sendtext("Testing Telegram bot")
+print(test)
