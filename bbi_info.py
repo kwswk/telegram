09abf6e4c8b8c3f868988ecdaf49244ee4f076fe
kWs <volvokung@gmail.com>
2020-11-20 02:09:32 +0800

    inline button testing

--- bbi_info.py
+++ bbi_info.py
@@ -1,19 +1,15 @@
 import requests
 import json
-from tabulate import tabulate
-import pandas as pd
 
-def get_bbi_info(route: str, direction: str) -> str:
-    conn = requests.get(f'http://www.kmb.hk/ajax/BBI/get_BBI2-en.php?routeno={route}&bound={direction}')
-    bbi_dict = json.loads(conn.text)
 
-    terminus = bbi_dict['bus_arr'][0]['dest']
-    bbi_detail = bbi_dict['Records']
-    df_bbi = pd.DataFrame(bbi_detail)[
-        ['sec_routeno',
-         # 'sec_dest',
-         # 'xchange',
-         'discount_max'
-         ]
-    ].set_index('sec_routeno')
-    return tabulate(df_bbi, tablefmt="pipe", headers="keys")
+def get_bbi_info(route: object) -> dict:
+    direction = ['F', 'B']
+    bbi_info = list()
+    for i in direction:
+        conn = requests.get(f'http://www.kmb.hk/ajax/BBI/get_BBI2-en.php?routeno={route}&bound={i}')
+        bbi_raw = json.loads(conn.text)
+        bbi_info.append(bbi_raw)
+
+    bbi_dict = {i['bus_arr'][0]['dest']: i['Records'] for i in bbi_info}
+
+    return bbi_dict
--- telegram_init.py
+++ telegram_init.py
@@ -1,5 +1,5 @@
-from uuid import uuid4
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram import replykeyboardmarkup, InlineKeyboardMarkup, InlineKeyboardButton
 from bbi_info import get_bbi_info
 
 
@@ -8,36 +8,8 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="hey bb, try my breast!")
 
 
-def put(update, context):
-    """Usage: /put value"""
-    # Generate ID and seperate value from command
-    key = str(uuid4())
-    # We don't use context.args here, because the value may contain whitespaces
-    value = update.message.text.partition(' ')[2]
-
-    # Store value
-    context.user_data[key] = value
-
-    update.message.reply_text(key)
-
-
-def get(update, context):
-    """Usage: /get uuid"""
-    # Seperate ID from command
-    key = context.args[0]
-
-    # Load value
-    value = context.user_data.get(key, 'Not found')
-    update.message.reply_text(value)
-
-
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=f"{update.message.text}")
-
-
-def caps(update, context):
-    text_caps = ' '.join(context.args).upper()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 
 def unknown(update, context):
@@ -46,24 +18,66 @@ def unknown(update, context):
 
 def bbi(update, context):
     route = context.args[0]
-    direction = context.args[1]
+    bbi_dict = get_bbi_info(route)
+    btn_directions = list(bbi_dict.keys())
+
+    lst_button = list()
+    for i in btn_directions:
+        lst_button.append(InlineKeyboardButton(i, callback_data=str(i)))
+
+    reply_markup = InlineKeyboardMarkup([lst_button])
+
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=get_bbi_info(route, direction)
+                             text=f'Rt. {route} ,, Choose Direction',
+                             reply_markup=reply_markup
                              )
 
 
-if __name__ == '__main__':
+def bbi_reply(update, context):
+    """handle BBI callback"""
+    if update.callback_query.message.text.split(' ')[0] == 'Rt.':
+        route = update.callback_query.message.text.split(' ')[1]
+        destination = update.callback_query.data
+        bbi_dict = get_bbi_info(route)[destination]
+        lst_chg = [i['sec_routeno'] for i in bbi_dict]
 
+        lst_button = list()
+        for i in lst_chg:
+            lst_button.append(InlineKeyboardButton(i, callback_data=str(i)))
+
+        reply_markup = InlineKeyboardMarkup([lst_button])
+
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=f'You Can Change the following routes - Rt. {route}',
+                                 reply_markup=reply_markup
+                                 )
+    elif update.callback_query.message.text[:35] == 'You Can Change the following routes':
+
+        route_chosen = update.callback_query.data
+        change_dir = [i['sec_dest'] for i in lst_chg if i['sec_routeno'] == route_chosen].pop
+        change_dest = [i['xchange'] for i in lst_chg if i['sec_routeno'] == route_chosen].pop
+        fare_detail = [i['discount_max'] for i in lst_chg if i['sec_routeno'] == route_chosen].pop
+
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=f'Change {route_chosen} to {change_dir}',
+                                 )
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=f'Change Point : {change_dest}',
+                                 )
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=f'Detail : {fare_detail}',
+                                 )
+
+
+if __name__ == '__main__':
     updater = Updater('1460342157:AAH26oIP5bwvL6SMgx3lE4BsiQLnAiidGQ8', use_context=True)
 
     """add functions to dispatcher"""
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(CommandHandler('put', put))
-    dp.add_handler(CommandHandler('get', get))
-    dp.add_handler(CommandHandler('caps', caps))
     dp.add_handler(CommandHandler('bbi', bbi))
-    dp.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
+    dp.add_handler(CallbackQueryHandler(bbi_reply))
+    # dp.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
     """Unknown handler"""
     dp.add_handler(MessageHandler(Filters.command, unknown))
 
