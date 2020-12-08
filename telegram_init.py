e8d0e459c4723c1cfc11c9a35acfdfab51d6c1a1
kWs <volvokung@gmail.com>
2020-12-08 23:32:58 +0800

    schedule message

--- telegram_init.py
+++ telegram_init.py
@@ -1,39 +1,62 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
-from telegram import replykeyboardmarkup, InlineKeyboardMarkup, InlineKeyboardButton
-from bbi_info import get_bbi_info
-from typing import List, Dict, Any
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, callbackcontext
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
 # initialize response list
+from py_func.bbi_info import get_bbi_info
+from py_func.layout_config import build_menu
+
+updater = Updater('1460342157:AAH26oIP5bwvL6SMgx3lE4BsiQLnAiidGQ8', use_context=True)
+timer = updater.job_queue
+
 bbi_res_lst = dict()
-
-
-def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
-    """fix layout of long list of inline buttons"""
-    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
-    if header_buttons:
-        menu.insert(0, header_buttons)
-    if footer_buttons:
-        menu.append(footer_buttons)
-    return menu
 
 
 def start(update, context):
     """/start"""
-    context.bot.send_message(chat_id=update.effective_chat.id, text="hey bb, try my breast!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Hi BB, this is our AI reminder')
+    updater.job_queue.run_repeating(date_counter, interval=86400, context=update.effective_chat.id)
+
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Unknown Command')
 
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=f"{update.message.text}")
 
 
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+# Periodic reminder
+def date_counter(context: callbackcontext):
+    today = datetime.now()
+    anniversary = datetime(2019, 9, 6)
+    meet_date = datetime(2019, 5, 23)
+    first_date = datetime(2019, 6, 9)
+
+    context.bot.send_message(chat_id=context.job.context, text=f'❤❤❤❤❤ Daily Summary {datetime.now()} ❤❤❤❤❤')
+    context.bot.send_message(
+        chat_id=context.job.context,
+        text=f'Happy {(today-anniversary).days} Days!'
+    )
+    context.bot.send_message(
+        chat_id=context.job.context,
+        text=f'{relativedelta(today,meet_date).years * 12 + relativedelta(today,meet_date).months} '
+             f'months since first chat '
+    )
+    context.bot.send_message(
+        chat_id=context.job.context,
+        text=f'{relativedelta(today,first_date).years * 12 + relativedelta(today,first_date).months} '
+             f'months since first date '
+    )
+    context.bot.send_message(
+        chat_id=context.job.context,
+        text=f'{relativedelta(today, anniversary).years * 12 + relativedelta(today, anniversary).months} '
+             f'months since anniversary '
+    )
 
 
-def bbi_res_hist(scope, response):
-    bbi_res_lst[scope] = response
-
-
+# BBI function
 def bbi_command_handler(update, context):
     route = context.args[0]
     bbi_dict = get_bbi_info(route)
@@ -54,8 +77,8 @@ def bbi_command_handler(update, context):
         text=f'/bbi-sel Select Direction',
         reply_markup=reply_markup
     )
-    bbi_res_hist('route', route)
-    bbi_res_hist('detail', bbi_dict)
+    bbi_res_lst['route'] = route
+    bbi_res_lst['detail'] = bbi_dict
 
 
 def bbi_callback_handler(update, context):
@@ -69,7 +92,7 @@ def bbi_callback_handler(update, context):
         )
 
         bbi_dict = bbi_res_lst['detail'][destination]
-        bbi_res_hist('detail', bbi_dict)
+        bbi_res_lst['detail'] = bbi_dict
 
         lst_chg = [i['sec_routeno'] for i in bbi_dict]
 
@@ -77,7 +100,7 @@ def bbi_callback_handler(update, context):
         for i in lst_chg:
             lst_button.append(InlineKeyboardButton(i, callback_data=str(i)))
 
-        reply_markup = InlineKeyboardMarkup(build_menu(lst_button, n_cols=3))
+        reply_markup = InlineKeyboardMarkup(build_menu(lst_button, n_cols=5))
         context.bot.send_message(
             chat_id=update.effective_chat.id,
             text=f'You Can Change the following routes'
@@ -111,7 +134,6 @@ def bbi_callback_handler(update, context):
 
 
 if __name__ == '__main__':
-    updater = Updater('1460342157:AAH26oIP5bwvL6SMgx3lE4BsiQLnAiidGQ8', use_context=True)
 
     """add functions to dispatcher"""
     dp = updater.dispatcher
@@ -121,7 +143,11 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler('bbi', bbi_command_handler))
     dp.add_handler(CallbackQueryHandler(bbi_callback_handler))
 
+    # dp.add_handler(CommandHandler('dating', date_counter, pass_job_queue=True))
+
     """Unknown handler"""
     dp.add_handler(MessageHandler(Filters.command, unknown))
 
     updater.start_polling()
+
+
